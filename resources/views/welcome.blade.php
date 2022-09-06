9a693ed591b865e8b0820a3b6059ac86a0a58856
btsutskiridze <bakatsutskiridze@redberry.ge>
2022-09-06 13:11:52 +0400

    initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Laravel</title>
+    @vite('resources/css/app.css')
+    <style>
+        body {
+            font-family: 'Nunito', sans-serif;
+        }
+    </style>
+</head>
+
+<body class="antialiased grid place-items-center h-screen">
+    <h1 class="">Its coronatime</h1>
+</body>
+
+</html>
