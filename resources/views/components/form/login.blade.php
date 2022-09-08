2f16eaa2340c3bc823f00a11025a1e341722a881
btsutskiridze <bakatsutskiridze@redberry.ge>
2022-09-08 21:40:16 +0400

    feat:  create login page mark up

---
+++ resources/views/components/form/login.blade.php
@@ -0,0 +1,52 @@
+<div class=" w-full lg:w-[470px] ">
+
+    <div>
+        <h2 class="mt-6 text-2xl tracking-tight font-black text-[#010414]">Welcome back</h2>
+        <p class="mt-2 text-sm md:text-xl text-gray-600">
+            Welcome back! Please enter your details
+        </p>
+    </div>
+
+    <div class="mt-8">
+
+        <div class="mt-6">
+            <form action="#" method="POST" class="space-y-6">
+                <div>
+                    <label for="username" class="block text-sm md:text-base font-bold text-[#010414]">Username</label>
+                    <div class="mt-1">
+                        <input id="username" name="username" type="name" autocomplete="username" required
+                            class="block w-full appearance-none rounded-md border border-gray-300 px-3 py-[1.14rem] placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm ">
+                    </div>
+                </div>
+
+                <div class="space-y-1">
+                    <label for="password" class="block text-sm md:text-base font-bold text-[#010414]">Password</label>
+                    <div class="mt-1">
+                        <input id="password" name="password" type="password" autocomplete="current-password" required
+                            class="block w-full appearance-none rounded-md border border-gray-300 px-3 py-[1.14rem] placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm">
+                    </div>
+                </div>
+
+                <div class="flex items-center justify-between">
+                    <div class="flex items-center">
+                        <input id="remember-me" name="remember-me" type="checkbox"
+                            class="h-4 w-4 rounded border-gray-300 text-green-700 focus:ring-green-500">
+                        <label for="remember-me" class="ml-2 block text-sm text-gray-900">Remember
+                            me</label>
+                    </div>
+
+                    <div class="text-sm">
+                        <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">Forgot password?</a>
+                    </div>
+                </div>
+
+                <div>
+                    <button type="submit"
+                        class="flex w-full justify-center rounded-lg border border-transparent bg-[#0FBA68] py-[1.14rem] px-4 md:text-base text-sm font-black uppercase text-white shadow-sm hover:bg-green-600">Log
+                        in</button>
+                </div>
+            </form>
+        </div>
+    </div>
+
+</div>
--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,4 +1,4 @@
-<header x-data='{show:false}' class="border-solid border-b-2 border-[#F6F6F7]">
+<header x-data='{show:false}' class="border-solid border-b-2  border-[#F6F6F7]">
     <div class="md:w-4/5 mx-auto">
         <x-navbar.pc />
         <x-navbar.mobile />
