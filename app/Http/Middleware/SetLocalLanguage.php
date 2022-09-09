7907d21f2d8302eef98ad94a0ed72fbb899add6d
btsutskiridze <bakatsutskiridze@redberry.ge>
2022-09-09 11:10:03 +0400

    fix: return type

--- app/Http/Middleware/SetLocalLanguage.php
+++ app/Http/Middleware/SetLocalLanguage.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\App;
@@ -17,7 +18,7 @@ class SetLocalLanguage
 	 *
 	 * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
 	 */
-	public function handle(Request $request, Closure $next): Response
+	public function handle(Request $request, Closure $next): Response|RedirectResponse
 	{
 		$lang = session('lang', 'en');
 		App::setLocale($lang);
