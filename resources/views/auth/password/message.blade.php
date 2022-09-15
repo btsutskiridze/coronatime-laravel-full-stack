1c7c608bc0749e177aa98283a66890dd8128714e
btsutskiridze <bakatsutskiridze@redberry.ge>
2022-09-15 19:46:52 +0400

    fix: email message responsivity

--- resources/views/auth/password/message.blade.php
+++ resources/views/auth/password/message.blade.php
@@ -17,16 +17,16 @@
     </style>
 </head>
 
-<body style="height: 100%; display: grid; place-items: center">
+<body style="height: 100%; text-align: center">
     <div style="padding: 30px">
-        <img src="https://i.ibb.co/dLb6CwL/landing.png" alt="landing" style="width: 100%; object-fit: contain" />
+        <img src="https://i.ibb.co/dLb6CwL/landing.png" alt="landing" style="max-width: 500px; object-fit: cover" />
         <h1
             style="font-style: normal; font-weight: 900; font-size: 25px; line-height: 30px; text-align: center; margin-top: 56px; margin-bottom: 16px">
             Recover password</h1>
-        <p style="font-weight: 400; font-size: 18px; line-height: 22px; text-align: center">click this button to recover
+        <p style="font-weight: 400; font-size: 18px; line-height: 22px; text-align: center; margin: 16px 0 40px 0">click
+            this button to recover
             a password</p>
-        <a href="{{ route('reset_password.reset', $token) }}"
-            style="display: flex; margin-top: 40px; justify-content: center; text-decoration: none">
+        <a href="{{ route('reset_password.reset', $token) }}" style="text-decoration: none">
             <button
                 style="
                         cursor: pointer;
@@ -37,7 +37,7 @@
                         font-size: 16px;
                         line-height: 19px;
                         text-align: center;
-                        padding: 19px 28%;
+                        padding: 19px 12%;
                         outline: none;
                         border: none;
                     ">
