35db118ec45989cef893a001344ee4bef1fb6fd7
btsutskiridze <bakatsutskiridze@redberry.ge>
2022-09-15 12:09:05 +0400

    fix: blades was not fully responsive

--- resources/views/auth/email/verified.blade.php
+++ resources/views/auth/email/verified.blade.php
@@ -4,23 +4,25 @@
             <div class="mt-10 mb-14 relative justify-self-center self-start">
                 <img src="{{ asset('images/logo.svg') }}" alt="logo">
             </div>
-            <div class="relative flex flex-col bottom-20 items-center justify-self-center self-start justify-center">
-                <img src="{{ asset('images/checked.svg') }}" alt="checked">
-                <p class="text-lg text-[#010414] text-center mt-4">
-                    Your account is confirmed, you can sign in
-                </p>
+            <div class="w-[90%] mx-auto ">
+                <div class="relative flex flex-col bottom-20 items-center justify-self-center self-start justify-center">
+                    <img src="{{ asset('images/checked.svg') }}" alt="checked">
+                    <p class="text-lg text-[#010414] text-center mt-4">
+                        Your account is confirmed, you can sign in
+                    </p>
 
-                <div class="w-96 mt-16">
-                    <a href="{{ route('worldwide.show') }}">
-                        <button
-                            class="flex w-full justify-center rounded-lg border border-transparent
+                    <div class="w-[100%] sm:w-96 mt-16">
+                        <a href="{{ route('worldwide.show') }}">
+                            <button
+                                class="flex w-full justify-center rounded-lg border border-transparent
                                         bg-[#0FBA68] py-[1.14rem] px-4 mb-6 md:text-base text-sm font-black 
                                         uppercase text-white shadow-sm hover:bg-green-600">
-                            Sign Up
-                        </button>
-                    </a>
-                </div>
+                                Sign Up
+                            </button>
+                        </a>
+                    </div>
 
+                </div>
             </div>
         </div>
     </x-slot>
